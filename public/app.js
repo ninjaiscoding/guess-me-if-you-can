88150d40fa9c0d44b51095fa0ef48e35f4b8f205
Aryan <114850025+ninjaiscoding@users.noreply.github.com>
2026-07-25 21:40:29 +0530

    Update app.js

--- public/app.js
+++ public/app.js
@@ -131,7 +131,7 @@ joinBtn.addEventListener('click', () => {
 });
 
 // Setup Category Options
-const categories = ["Anime Characters", "Animals", "Real Famous People", "Video Game Characters", "Sports Persons", "Superheroes", "Cartoon Characters", "Movie Characters", "Mythological Creatures", "Historical Figures", "Musicians & Singers", "Vehicles", "Villains", "Sci-Fi Characters", "Fantasy Characters", "Comedians", "Internet Personalities", "Wrestlers", "Space", "Board Game/Toy Characters"];
+const categories = ["Anime Characters", "Animals", "Real Famous People", "countries and cities", "Sports Persons", "Superheroes", "Cartoon Characters", "Movie Characters", "Mythological Creatures", "Historical Figures", "Musicians & Singers", "Vehicles", "Villains", "Sci-Fi Characters", "Fantasy Characters", "Comedians", "Youtubers", "Professions & Jobs", "Space", "video Games"];
 categories.forEach(c => {
     const opt = document.createElement('option');
     opt.value = c;
